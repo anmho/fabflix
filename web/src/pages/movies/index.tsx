65b07b554bb60e3046ef65eb053971296a82ea0d
Mehmet Nadi <mhmtalperennadi@gmail.com>
2024-04-08 12:17:59 -0700

    change text hover color again : )

--- web/src/pages/movies/index.tsx
+++ web/src/pages/movies/index.tsx
@@ -52,7 +52,7 @@ const MovieListPage: React.FC = () => {
                   translateZ={20}
                   as={Link}
                   href={`/movies/${movie.id}`}
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white hover:text-[#FBAD9C]"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white hover:text-blue-500"
                 >
                   Learn More →
                 </CardItem>
