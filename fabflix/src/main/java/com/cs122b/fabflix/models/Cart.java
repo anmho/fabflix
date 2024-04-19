ef129bfaa947eb5c2b19e3554554c63b2e752bc8
Mehmet Nadi <mhmtalperennadi@gmail.com>
2024-04-18 17:04:42 -0700

    define cart object

---
+++ fabflix/src/main/java/com/cs122b/fabflix/models/Cart.java
@@ -0,0 +1,51 @@
+package com.cs122b.fabflix.models;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Cart {
+    private ArrayList<Movie> movies;
+
+    public Cart() {
+        this.movies = new ArrayList<Movie>();
+    }
+    public Cart(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+    public void addMovie(Movie movie) {
+        this.movies.add(movie);
+    }
+    public void removeMovie(Movie movie) {
+        this.movies.remove(movie);
+    }
+    public ArrayList<Movie> getMovies() {
+        return this.movies;
+    }
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+    public boolean updateMovieQuantity(String movieId, int quantity) {
+        if (quantity <= 0) {
+            return this.removeMovieById(movieId);
+        }
+        for (Movie m : this.movies) {
+            if (m.getId().equals(movieId)) {
+                m.setQuantity(quantity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeMovieById(String movieId) {
+        Iterator<Movie> iterator = this.movies.iterator();
+        while (iterator.hasNext()) {
+            Movie m = iterator.next();
+            if (m.getId().equals(movieId)) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+}
