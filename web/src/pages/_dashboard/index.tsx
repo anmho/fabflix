790d65f83a0e1f34ce4c19ffdef7c6f1859931a2
Mehmet Nadi <mhmtalperennadi@gmail.com>
2024-05-03 11:05:16 -0700

    dashboard page init

---
+++ web/src/pages/_dashboard/index.tsx
@@ -0,0 +1,13 @@
+// At the top of your file
+import React, { useState } from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <div className="py-[50px] md:py-[80px] lg:py-[100px] xl:py-[120px] lg:px-[15%] md:px-[5%] flex flex-wrap justify-center items-start gap-4 p-4">
+        dashboard
+      </div>
+    </>
+  );
+};
+export default Dashboard;
