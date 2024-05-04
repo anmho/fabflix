94224a6f44539b50e1d0d1199bee6bc3981697b7
Mehmet Nadi <mhmtalperennadi@gmail.com>
2024-05-03 18:32:07 -0700

    delete unused compoent

--- web/src/pages/_dashboard/index.tsx
+++ web/src/pages/_dashboard/index.tsx
@@ -1,15 +1,12 @@
 // At the top of your file
 import React, { useState } from "react";
-import EmployeeOnly from "~/components/EmployeeOnly";
 
 const Dashboard: React.FC = () => {
   return (
     <>
-      <EmployeeOnly>
-        <div className="py-[50px] md:py-[80px] lg:py-[100px] xl:py-[120px] lg:px-[15%] md:px-[5%] flex flex-wrap justify-center items-start gap-4 p-4">
-          dashboard
-        </div>
-      </EmployeeOnly>
+      <div className="py-[50px] md:py-[80px] lg:py-[100px] xl:py-[120px] lg:px-[15%] md:px-[5%] flex flex-wrap justify-center items-start gap-4 p-4">
+        dashboard
+      </div>
     </>
   );
 };
