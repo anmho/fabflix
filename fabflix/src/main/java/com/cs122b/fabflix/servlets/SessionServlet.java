61f50a623109dfcf906758b2a33a163ba4d826e2
Andrew Ho <andyminhtuanho@gmail.com>
2024-06-06 18:26:14 -0700

    k8s init

--- fabflix/src/main/java/com/cs122b/fabflix/servlets/SessionServlet.java
+++
@@ -1,2 +0,0 @@
-package com.cs122b.fabflix.servlets;public class SessionServlet {
-}
---
+++ k8s/setup.sh
@@ -0,0 +1,5 @@
+helm install mysql --set auth.rootPassword=cs122b-root,auth.database=moviedb,auth.username=admin,auth.password=cs122b,secondary.persistence.enabled=true,secondary.persistence.size=2Gi,primary.persistence.enabled=true,primary.persistence.size=2Gi,architecture=replication,auth.replicationPassword=cs122b-replica,secondary.replicaCount=1,primary.resourcesPreset=medium,secondary.resourcesPreset=medium oci://registry-1.docker.io/bitnamicharts/mysql
+
+
+
+
