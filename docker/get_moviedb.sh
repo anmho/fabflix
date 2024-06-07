608a853b4f1913e74349d5ec55e7f0ecd41bf576
Andrew Ho <andyminhtuanho@gmail.com>
2024-06-07 16:12:11 -0700

    docker cleanup. need to fix strange cors issue

--- docker/get_moviedb.sh
+++ docker/get_moviedb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "https://raw.githubusercontent.com/UCI-Chenli-teaching/cs122b-s24-team-420/k8s/k8s/moviedb.sql?token=GHSAT0AAAAAACBAFN4BLISA4XTMGNG5GK7IZTDQLNA" > moviedb.sql
+curl "https://raw.githubusercontent.com/anmho/cs122b-data/main/moviedb.sql" > ./data/moviedb.sql
